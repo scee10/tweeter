8d826729af5d2f7a9e716e29b82ed393e30f7520
scee10 <schhor05@gmail.com>
2022-03-02 20:32:32 +0000

    feat: able to get posts and now display on page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,32 +8,6 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-
-const tweetData = [
- {
-   "user": {
-     "name": "Newton",
-     "avatars": "https://i.imgur.com/73hZDYK.png"
-     ,
-     "handle": "@SirIsaac"
-   },
-   "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-   "created_at": 1461116232227
- },
- {
-   "user": {
-     "name": "Descartes",
-     "avatars": "https://i.imgur.com/nlhLi3I.png",
-     "handle": "@rd" },
-   "content": {
-     "text": "Je pense , donc je suis"
-   },
-   "created_at": 1461113959088
- }
-]
-
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -74,14 +48,19 @@ const createTweetElement = function(tweet) {
 
 const loadTweets = function () {
 
+ $.ajax({method: "GET", url: '/tweets', dataType: 'JSON'})
+  .then(posts => {
+   renderTweets(posts)
+  })
+  .catch(err => {console.log('err', error)}) 
+
 }
 
 $(document).ready(function () {
- // rendering tweets on browser
- renderTweets(tweetData);
+ 
+ loadTweets();
 
  const $form = $('.submit-form')
-
  $form.submit(function(event) {
   // prevent the post request
   event.preventDefault();
